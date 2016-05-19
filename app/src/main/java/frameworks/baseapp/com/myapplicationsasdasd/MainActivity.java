6aec64d96ee8eb304a9c9955e21080c9cbc31bd1
wby <www.731173809@qq.com>
2016-05-19 15:15:42 +0800

    Initial commit

---
+++ app/src/main/java/frameworks/baseapp/com/myapplicationsasdasd/MainActivity.java
@@ -0,0 +1,13 @@
+package frameworks.baseapp.com.myapplicationsasdasd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/frameworks/baseapp/com/myapplicationsasdasd/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package frameworks.baseapp.com.myapplicationsasdasd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
